793e0e510d26315bc401e4ac2166fccd471e5526
Joe Lambon <joe.lambon25@gmail.com>
2025-09-03 13:22:38 +0100

    add Slide animation for ImageListItems

--- src/components/standardImageList.tsx
+++ src/components/standardImageList.tsx
@@ -1,56 +1,55 @@
 import { antiqueItemData as antiques } from '../common/variables';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import { ImageListItemBar, useMediaQuery, Slide } from '@mui/material';
+import { ImageListItemBar, useMediaQuery, Slide, Box } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
-import useOnScreen from '../helpers/useOnScreen';
+import useOnScreen from '../hooks/useOnScreen';
 
 export default function StandardImageList( ) {
     const theme = useTheme();
     const onMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-    
     return (        
         <ImageList
         sx={{
-            width: '100%',
-            height: '100%'
+            overflowY: 'hidden'
         }}
         cols={onMobile ? 1 : 3}
-        gap={10} >
+        gap={onMobile ? 20 : 10} >
             {antiques.map((item, index) => {
                 const [containerRef, isVisible] = useOnScreen({
                     root: null,
                     rootMargin: "0px",
-                    threshold: 0.1
+                    threshold: onMobile ? 0.1 : 0
                 })
+
                 return (
                     <Slide
-                    key={`slide_${index}`}
-                    mountOnEnter
-                    unmountOnExit
-                    direction='up'
-                    timeout={500}
-                    in={isVisible}>
-                        <ImageListItem 
-                        ref={containerRef}
-                        sx={{
-                            '&:hover': {
-                                cursor: 'pointer',
-                            },
-                        }}
-                        >
-                            <img
-                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                        <ImageListItemBar
-                                title={item.title}
-                            />
-                        </ImageListItem>
+                        key={`wrapper_antique_${index}`}
+                        direction='up'
+                        timeout={onMobile ? 1000 : 1500}
+                        in={isVisible}
+                    >
+                        <Box ref={containerRef}>
+                            <ImageListItem 
+                            sx={{
+                                '&:hover': {
+                                    cursor: 'pointer',
+                                },
+                            }}
+                            >
+                                <img
+                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                    alt={item.title}
+                                    loading="lazy"
+                                />
+                            <ImageListItemBar
+                                    title={item.title}
+                                />
+                            </ImageListItem>
+                        </Box>
                     </Slide>
                 )
             })}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,6 @@
 import { Container, Typography } from '@mui/material';
-import AppBar from '../components/appBar';
-import StandardImageList from '../components/standardImageList';
+import AppBar from '../components/AppBar';
+import StandardImageList from '../components/StandardImageList';
 
 function Home() {
   return (
@@ -13,6 +13,17 @@ Everyone is entitled in full equality to a fair and public hearing by an indepen
 No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.
         </Typography>
         <StandardImageList />
+        <Typography 
+        sx={{
+          p: 5,
+          m: 3, 
+          textAlign: 'center',
+          fontSize:'48px', 
+          color: 'rgb(129, 129, 129)'
+        }}
+          >
+          Footer placeholder
+        </Typography>
       </Container>
     </>
   )
