b4057346bf4332083b9a98b3f606d8be25e0e72a
Joe Lambon <joe.lambon25@gmail.com>
2025-09-03 06:55:27 +0100

    WIP - add slide transitions

--- src/components/standardImageList.tsx
+++ src/components/standardImageList.tsx
@@ -1,41 +1,59 @@
 import { antiqueItemData as antiques } from '../common/variables';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import { ImageListItemBar, useMediaQuery } from '@mui/material';
+import { ImageListItemBar, useMediaQuery, Slide } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+
+import useOnScreen from '../helpers/useOnScreen';
 
 export default function StandardImageList( ) {
     const theme = useTheme();
     const onMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  
-    return (
-    <ImageList
+
+    
+    return (        
+        <ImageList
         sx={{
             width: '100%',
             height: '100%'
         }}
         cols={onMobile ? 1 : 3}
         gap={10} >
-            {antiques.map((item) => (
-                <ImageListItem 
-                key={item.title}
-                sx={{
-                    '&:hover': {
-                        cursor: 'pointer',
-                    },
-                }}
-                >
-                <img
-                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                    alt={item.title}
-                    loading="lazy"
-                />
-                <ImageListItemBar
-                    title={item.title}
-                />
-                </ImageListItem>
-            ))}
+            {antiques.map((item, index) => {
+                const [containerRef, isVisible] = useOnScreen({
+                    root: null,
+                    rootMargin: "0px",
+                    threshold: 0.1
+                })
+                return (
+                    <Slide
+                    key={`slide_${index}`}
+                    mountOnEnter
+                    unmountOnExit
+                    direction='up'
+                    timeout={500}
+                    in={isVisible}>
+                        <ImageListItem 
+                        ref={containerRef}
+                        sx={{
+                            '&:hover': {
+                                cursor: 'pointer',
+                            },
+                        }}
+                        >
+                            <img
+                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                        <ImageListItemBar
+                                title={item.title}
+                            />
+                        </ImageListItem>
+                    </Slide>
+                )
+            })}
     </ImageList>
   );
 }
--- src/util/links.tsx
+++
@@ -1,6 +0,0 @@
-const links : string [] = [
-    "https://images.squarespace-cdn.com/content/v1/5fc40e2e2bbd7406581a8fad/d48d6a7b-6454-4222-b275-f99aaf4c1cdb/Shellwork_Horse_6.jpg?format=1500w",
-    "https://images.squarespace-cdn.com/content/v1/5fc40e2e2bbd7406581a8fad/b406a358-335d-479d-a928-31906f9c9428/Pair_of_Circular_Dog_Oil_on_Canvas_11.jpg?format=1500w",
-    "https://images.squarespace-cdn.com/content/v1/5fc40e2e2bbd7406581a8fad/f1424457-5413-4f4e-a2a5-106aa4968903/Coffee%2C+Tea%2C+Tobacco+Snuff%2C+Vinegar_4.jpg?format=1500w"
-
-]
