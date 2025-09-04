948401220d3f290fa67e3376d900c7b26ccdd099
Joe Lambon <joe.lambon25@gmail.com>
2025-09-04 19:11:25 +0100

    swap Slide animation for Fade

--- src/components/standardImageList.tsx
+++ src/components/standardImageList.tsx
@@ -1,7 +1,7 @@
 import { antiqueItemData as antiques } from '../common/variables';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import { ImageListItemBar, useMediaQuery, Slide, Box } from '@mui/material';
+import { ImageListItemBar, useMediaQuery, Fade, Box } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 import useOnScreen from '../hooks/useOnScreen';
@@ -12,8 +12,10 @@ export default function StandardImageList( ) {
 
     return (   
             <ImageList
+            id='test-element-root-ref'
             sx={{
-                overflowY: 'hidden'
+                overflowY: 'visible',
+                height: 'auto'
             }}
             cols={onMobile ? 1 : 3}
             gap={onMobile ? 20 : 10}
@@ -21,51 +23,54 @@ export default function StandardImageList( ) {
                 {antiques.map((item, index) => {
                     const [containerRef, isVisible] = useOnScreen({
                         root: null,
-                        rootMargin: "0px",
-                        threshold: onMobile ? 0.2 : 0
+                        rootMargin: "0px 0px 0px 0px",
+                        threshold: onMobile ? 0.2 : 0.8
                     })
-                    return (
-                        <Slide
+                    return (    
+                        <Fade
                         key={`wrapper-antique-${index}`}
-                        direction='up'
-                        timeout={onMobile ? 1000 : 1500}
+                        appear={true}        
+                        timeout={onMobile ? 500 : 1500}
                         in={isVisible}
                         >
-                            <ImageListItem
+                            <Box
                             ref={containerRef}
-                            sx={{
-                                '&:hover': {
-                                    cursor: 'pointer',
-                                }
-                            }}
                             >
-                                <img
-                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
+                                <ImageListItem
+                                sx={{
+                                    '&:hover': {
+                                        cursor: 'pointer',
+                                    }               
+                                }}
+                                >
+                                    <img
+                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                        alt={item.title}
+                                        loading="lazy"
+                                    />
 
-                                <Box
-                                display={onMobile ? 'flex' : 'none'}
-                                >
-                                    <ImageListItemBar
-                                    title={item.title}
-                                    subtitle='£50'
-                                    position={'below'}
-                                    />                                        
-                                </Box>
-                                <Box
-                                display={onMobile ? 'none' : 'flex'}
-                                >
-                                    <ImageListItemBar
-                                    title={item.title}
-                                    subtitle='£50'
-                                    position={'bottom'}
-                                    />                                        
-                                </Box>
-                            </ImageListItem>    
-                        </Slide>
+                                    <Box
+                                    display={onMobile ? 'flex' : 'none'}
+                                    >
+                                        <ImageListItemBar
+                                        title={item.title}
+                                        subtitle='£50'
+                                        position={'below'}
+                                        />                                        
+                                    </Box>
+                                    <Box
+                                    display={onMobile ? 'none' : 'flex'}
+                                    >
+                                        <ImageListItemBar
+                                        title={item.title}
+                                        subtitle='£50'
+                                        position={'bottom'}
+                                        />                                        
+                                    </Box>
+                                </ImageListItem>    
+                            </Box>
+                        </Fade>
                     )
                 })}
             </ImageList>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,7 +7,7 @@ function Home() {
     <>
       <AppBar />
       <Container maxWidth='lg' >
-        <Typography sx={{mt: 3, textAlign: 'center', fontSize:'21px', color: 'rgb(129, 129, 129)'}}>
+        <Typography sx={{ height: '50vh',mt: 3, textAlign: 'center', fontSize:'21px', color: 'rgb(129, 129, 129)'}}>
           No one shall be subjected to arbitrary arrest, detention or exile.
 Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him.
 No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.
