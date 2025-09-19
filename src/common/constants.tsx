33e481eb9a76e460a6ae0fb1c8dd09b0b8cb59a5
Joe Lambon <joe.lambon25@gmail.com>
2025-09-19 16:31:37 +0100

    fix spacing & initial render

--- src/common/constants.tsx
+++ src/common/constants.tsx
@@ -5,7 +5,7 @@ export const CONSTANTS = {
     MOBILE: {
         GALLERY_COLUMNS: 2,
         GALLERY_GAP: 5,
-        SCREEN_THRESHOLD: 0.7
+        SCREEN_THRESHOLD: 0.6
     },
     
     LARGER_SCREENS: {
