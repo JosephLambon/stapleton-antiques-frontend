beeb03f60ca224181a09adbfc8a1b239f908ed43
Joe Lambon <joe.lambon25@gmail.com>
2025-11-05 21:09:30 +0000

    add skeletons

--- src/components/AntiqueGallery/AntiqueGallery.tsx
+++ src/components/AntiqueGallery/AntiqueGallery.tsx
@@ -2,13 +2,16 @@ import { antiqueItemData as antiques } from '../../common/variables';
 import { 
     AntiqueGalleryWrapper,
 } from '../AntiqueGallery/AntiqueGallery.styling';
-import { useMediaQuery } from '@mui/material';
+import { Skeleton, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import CONSTANTS from '../../common/constants';
 import { GetAntiquesByStatus } from '../../services/antiqueService';
 import { useQuery } from "@tanstack/react-query";
 
 import { AntiqueGalleryItem } from './AntiqueGalleryItem';
+import { AntiqueGallerySkeleton } from "./AntiqueGallerySkeleton";
+
+const SKELETON_COUNT = 8;
 
 export default function AntiqueGallery( ) {
     const theme = useTheme();
@@ -21,8 +24,8 @@ export default function AntiqueGallery( ) {
         } = useQuery({
           queryKey: ["antiquesData"],
           queryFn: () => GetAntiquesByStatus(true,true,true),
-          gcTime: 5 * 60 * 1000,          // Bin cache after 5 mins,
-          staleTime: 1 * 60 * 1000        // Data considered stale after 1 min, will refetch & update cache in background
+          gcTime: 60 * 60 * 1000,          // Bin cache after 5 mins,
+          staleTime: 30 * 60 * 1000        // Data considered stale after 30 min, will refetch & update cache in background
       });
 
     return (   
@@ -31,15 +34,22 @@ export default function AntiqueGallery( ) {
             gap={onMobile ? CONSTANTS.MOBILE.GALLERY_GAP : CONSTANTS.LARGER_SCREENS.GALLERY_GAP}
             rowHeight={300}
             >
-                {
-                isLoading? <p>Fetching posts...</p> : 
-                antiques.map((antique) => (
-                    <AntiqueGalleryItem
-                    key={antique.id}
-                    item={antique}
-                    onMobile={onMobile}
-                    />
-                ))}
+              {
+                 isLoading? (
+                    Array.from({ length: SKELETON_COUNT }).map((_, idx) => (
+                      <AntiqueGallerySkeleton key={idx} />
+                    ))
+                  ) : (
+                  // error? <p>Error fetching posts.</p> : (
+                  antiques.map((antique) => (
+                      <AntiqueGalleryItem
+                      key={antique.id}
+                      item={antique}
+                      onMobile={onMobile}
+                      />
+                  )))
+                // )
+              }
             </AntiqueGalleryWrapper>
   );
 }
