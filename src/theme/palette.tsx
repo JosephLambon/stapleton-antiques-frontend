959493dfb8da0740a636424b8b68a5857fd2fdf9
Joe Lambon <joe.lambon25@gmail.com>
2025-11-06 18:49:22 +0000

    update theme

--- src/theme/palette.tsx
+++ src/theme/palette.tsx
@@ -2,14 +2,19 @@ import { type ThemeOptions } from "@mui/material/styles";
 
 export const palette : ThemeOptions["palette"] = {
     primary: {
-        main: 'rgb(0, 17, 94)'
+        main: 'rgba(4, 17, 78, 1)'
     },
     secondary: {
-        main: 'rgb(251, 249, 249)'
+        // main: 'rgb(251, 249, 249)'
+        main: 'rgba(4, 17, 78, 1)'
     },
     text: {
-        primary: 'rgba(0, 0, 0, 0.87)',
-        secondary: 'rgba(0, 0, 0, 0.6)',
-        disabled: 'rgba(0, 0, 0, 0.38)'
+        // primary: 'rgba(0, 0, 0, 0.87)',
+        // secondary: 'rgba(0, 0, 0, 0.6)',
+        // disabled: 'rgba(0, 0, 0, 0.38)'
+        
+        primary: 'rgba(251, 249, 249, 1)',
+        secondary: 'rgba(251, 249, 249, 1)',
+        disabled: 'rgb(251, 249, 249)'
     }
 };
