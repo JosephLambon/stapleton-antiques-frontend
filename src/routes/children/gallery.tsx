5aea6b62680f185b530dbe0fc36c5dc2f043ae27
Joe Lambon <joe.lambon25@gmail.com>
2025-11-16 15:44:54 +0000

    hotfix2 - style landing background for demo

--- src/routes/children/gallery.tsx
+++ src/routes/children/gallery.tsx
@@ -1,14 +1,18 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import AntiqueGallery from '../../components/AntiqueGallery/AntiqueGallery';
 import { OutletContainer } from '../../components/Common/Common.styling';
 
 const Gallery = () => {
+  const theme = useTheme();
+  const onMobile: boolean = useMediaQuery(theme.breakpoints.down('sm'));
+  
   return (
       <>
       <Box
         sx={{
           width: '100%',
-          height: '80vh',
+          height: '78vh',
+          marginTop: '-5vh',
           backgroundImage: 'url(https://sastapletonantiques.blob.core.windows.net/antique-image-container/background.png)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
@@ -16,14 +20,12 @@ const Gallery = () => {
           display: 'flex',
           justifyContent:'center',
           alignItems: 'end',
-          pb: 25,
-          mb: 5
+          pb: onMobile ? 20 : 10
         }}
       >
         <Typography variant='h2' color='textPrimary'
           sx={{
-            textShadow: '-15px 5px 20px #000000ff',
-            px: 1
+            textShadow: '-15px 5px 20px #000000ad'
           }}
         >
           Decorative Antiques
